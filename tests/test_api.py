06436b966aafa2feddd3b70ed78bb820ff4fd462
Andy Xie <andy.xie.opensource@gmail.com>
2024-01-24 18:42:01 +0800

    test_api.py: Added initial test data

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,13 +6,14 @@ from leetcode_0058_length_of_last_word import api
 
 
 @pytest.mark.parametrize(
-    ["result", ...],
+    ["result", "s"],
     (
-        [..., ...],
-        [..., ...],
-    )
+        [5, "Hello World"],
+        [4, "  test2 xxxxxx xxxxxxx xxxxxxxxx  "],
+        [6, "1 12 123 1234 12345"],
+    ),
 )
-def test_length_of_last_word(result, ...) -> None:
+def test_length_of_last_word(result, s) -> None:
     """Tests solution for problem Length of Last Word"""
 
-    assert api.length_of_last_word(...) == result
+    assert api.length_of_last_word(s) == result
