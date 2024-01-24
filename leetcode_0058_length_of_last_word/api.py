c47bfabb69589343e2ca3b3dee40a4a697f93e03
Andy Xie <andy.xie.opensource@gmail.com>
2024-01-24 19:06:35 +0800

    api.py: Add char checks

--- leetcode_0058_length_of_last_word/api.py
+++ leetcode_0058_length_of_last_word/api.py
@@ -1,7 +1,25 @@
 """API for solving problem Length of Last Word"""
 
-def _check_preconditions(...) -> bool:
+def _is_letter(c) -> bool:
     pass
+
+def _is_space(c) -> bool:
+    pass
+
+def _is_letter_or_space(c) -> bool:
+    pass
+
+def _check_preconditions(s) -> bool:
+    if not 1 <= len(s) <= 10**4:
+        return False
+    
+    if not all(_is_letter_or_space(c for c in s)):
+        return False
+    
+    if not any(_is_letter(c for c in s)):
+        return False
+    
+    return True
 
 
 def length_of_last_word(...) -> ...:
