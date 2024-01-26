fae5247bc0eeb91b6562d9ce6e25ec7282bd04d1
Andy Xie <andy.xie.opensource@gmail.com>
2024-01-26 21:39:25 +0800

    api.py: Add newlines and args

--- leetcode_0058_length_of_last_word/api.py
+++ leetcode_0058_length_of_last_word/api.py
@@ -1,36 +1,42 @@
 """API for solving problem Length of Last Word"""
 
+
 def _is_upper_letter(c) -> bool:
     return 65 <= ord(c) <= 90
+
 
 def _is_lower_letter(c) -> bool:
     return 97 <= ord(c) <= 122
 
+
 def _is_letter(c) -> bool:
     return _is_upper_letter(c) or _is_lower_letter(c)
+
 
 def _is_space(c) -> bool:
     return ord(c) == 32
 
+
 def _is_letter_or_space(c) -> bool:
     return _is_letter(c) or _is_space(c)
+
 
 def _check_preconditions(s) -> bool:
     if not 1 <= len(s) <= 10**4:
         return False
-    
+
     if not all(_is_letter_or_space(c for c in s)):
         return False
-    
+
     if not any(_is_letter(c for c in s)):
         return False
-    
+
     return True
 
 
-def length_of_last_word(...) -> ...:
+def length_of_last_word(s: str) -> int:
     """Solves problem Length of Last Word"""
 
-    assert _check_preconditions(...)
+    assert _check_preconditions(s)
 
     pass
