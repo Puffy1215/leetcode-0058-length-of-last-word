80a673566f1209c14da267c007a5838df2f87b5e
Andy Xie <andy.xie.opensource@gmail.com>
2024-01-28 12:00:04 +0800

    api.py: Specify types

--- leetcode_0058_length_of_last_word/api.py
+++ leetcode_0058_length_of_last_word/api.py
@@ -1,27 +1,27 @@
 """API for solving problem Length of Last Word"""
 
 
-def _is_upper_letter(c) -> bool:
+def _is_upper_letter(c: str) -> bool:
     return 65 <= ord(c) <= 90
 
 
-def _is_lower_letter(c) -> bool:
+def _is_lower_letter(c: str) -> bool:
     return 97 <= ord(c) <= 122
 
 
-def _is_letter(c) -> bool:
+def _is_letter(c: str) -> bool:
     return _is_upper_letter(c) or _is_lower_letter(c)
 
 
-def _is_space(c) -> bool:
+def _is_space(c: str) -> bool:
     return ord(c) == 32
 
 
-def _is_letter_or_space(c) -> bool:
+def _is_letter_or_space(c: str) -> bool:
     return _is_letter(c) or _is_space(c)
 
 
-def _check_preconditions(s) -> bool:
+def _check_preconditions(s: str) -> bool:
     if not 1 <= len(s) <= 10**4:
         return False
 
